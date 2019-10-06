90c0552a29b7f473a262109127df4c6d91fb2920
Jerry Ai <aijl2008@qq.com>
2019-10-06 15:23:11 +0800

    Update install-php-on-centos.sh

--- install-php-on-centos.sh
+++ install-php-on-centos.sh
@@ -1,12 +1,3 @@
-mkdir -p /data/src
-cd /data/src
-curl -o nginx-1.12.2.tar.gz -s http://nginx.org/download/nginx-1.12.2.tar.gz
-curl -o php-7.1.30.tgz -s https://www.php.net/distributions/php-7.1.30.tar.gz
-curl -o redis-3.1.6.tgz -s http://pecl.php.net/get/redis-3.1.6.tgz
-curl -o mongodb-1.5.5.tgz -s http://pecl.php.net/get/mongodb-1.5.5.tgz
-curl -o imagick-3.1.2.tgz -s http://pecl.php.net/get/imagick-3.1.2.tgz
-curl -o grpc-1.22.0.tgz -s http://pecl.php.net/get/grpc-1.22.0.tgz
-curl -o composer-installer.php -s https://getcomposer.org/installer
 curl -o /etc/yum.repos.d/CentOS-Base.repo -s http://mirrors.aliyun.com/repo/Centos-7.repo
 yum install -y epel-release
 rpm -ivh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
@@ -23,16 +14,60 @@ yum install -y libmcrypt-devel
 yum install -y libpng-devel
 yum install -y libjpeg-devel
 yum install -y libssh2-devel
-/usr/sbin/groupadd -f -g 501 www-data
 /usr/sbin/groupadd -f -g 510 php-fpm
-/usr/sbin/useradd -m -u 501 -g 501 www-data
 /usr/sbin/useradd -m -u 510 -g 510 php-fpm
+mkdir -p /data/src
 mkdir -p /data/webroot && chown www-data:www-data /data/webroot
 mkdir -p /data/webroot/runtimes && chown php-fpm:php-fpm /data/webroot/runtimes
 mkdir -p /data/logs/php
+cd /data/src
+curl -o php-7.1.30.tgz -s https://www.php.net/distributions/php-7.1.30.tar.gz
+curl -o redis-3.1.6.tgz -s http://pecl.php.net/get/redis-3.1.6.tgz
+curl -o mongodb-1.5.5.tgz -s http://pecl.php.net/get/mongodb-1.5.5.tgz
+curl -o imagick-3.1.2.tgz -s http://pecl.php.net/get/imagick-3.1.2.tgz
+curl -o grpc-1.22.0.tgz -s http://pecl.php.net/get/grpc-1.22.0.tgz
+curl -o composer-installer.php -s https://getcomposer.org/installer
 tar -zxvf php-7.1.30.tgz
 cd php-7.1.30
-./configure --prefix=/usr/local/php --enable-fpm --with-fpm-user=php-fpm --with-fpm-group=php-fpm --enable-sigchild --disable-short-tags --with-libxml-dir --with-openssl --with-pcre-regex --with-zlib --enable-calendar --with-curl --enable-exif --with-jpeg-dir --with-png-dir --with-freetype-dir --with-gd --enable-gd-native-ttf --with-gettext --with-mhash --enable-intl --enable-mbstring --with-mcrypt --with-mysqli --enable-pcntl --with-pdo-mysql --with-readline --enable-shmop --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --with-tidy --with-xmlrpc --with-xsl=DIR --enable-zip --enable-mysqlnd && \
+./configure \
+  --prefix=/usr/local/php \
+  --enable-fpm \
+  --with-fpm-user=php-fpm \
+  --with-fpm-group=php-fpm \
+  --enable-sigchild \
+  --disable-short-tags \
+  --with-libxml-dir \
+  --with-openssl \
+  --with-pcre-regex \
+  --with-zlib \
+  --enable-calendar \
+  --with-curl \
+  --enable-exif \
+  --with-jpeg-dir \
+  --with-png-dir \
+  --with-freetype-dir \
+  --with-gd \
+  --enable-gd-native-ttf \
+  --with-gettext \
+  --with-mhash \
+  --enable-intl \
+  --enable-mbstring \
+  --with-mcrypt \
+  --with-mysqli \
+  --enable-pcntl \
+  --with-pdo-mysql \
+  --with-readline \
+  --enable-shmop \
+  --enable-soap \
+  --enable-sockets \
+  --enable-sysvmsg \
+  --enable-sysvsem \
+  --enable-sysvshm \
+  --with-tidy \
+  --with-xmlrpc \
+  --with-xsl=DIR \
+  --enable-zip \
+  --enable-mysqlnd
 make && make install
 ln -s /usr/local/php/bin/* /usr/local/bin
 cp php.ini-production /usr/local/php/lib/php.ini
@@ -66,11 +101,6 @@ cd mongodb-1.5.5
 make && make install
 echo "extension=mongodb.so" >> /usr/local/php/lib/php.ini
 cd .. && rm -rf mongodb-1.5.5
-tar -zxvf nginx-1.12.2.tar.gz
-cd nginx-1.12.2
-./configure --prefix=/usr/local/nginx --error-log-path=/data/logs/nginx/error.log --http-log-path=/data/logs/nginx/access.log --user=www-data --group=www-data --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module  --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module && \
-make && make install
-cd ..  && rm -rf nginx-1.12.2
 cat > /usr/local/php/etc/php-fpm.conf <<EOF
 [global]
 error_log = /data/logs/php/fpm_errors.log
@@ -78,6 +108,7 @@ process.max = 128
 daemonize = yes
 include = etc/php-fpm.d/*.conf
 EOF
+mkdir -p /usr/local/php/etc/php-fpm.d
 cat > /usr/local/php/etc/php-fpm.d/pool.conf <<EOF
 [pool-1]
 user = php-fpm
@@ -92,49 +123,3 @@ access.log = /data/logs/php/$pool.access.log
 slowlog = /data/logs/php/$pool.log.slow
 request_slowlog_timeout = 2
 EOF
-
-cat > /usr/local/nginx/conf/nginx.conf  <<EOF
-worker_processes  2;
-events {
-    worker_connections  1024;
-}
-http {
-    include       mime.types;
-    default_type  application/octet-stream;
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-    sendfile        on;
-    keepalive_timeout  65;
-    include vhost/*.conf
-}
-EOF
-mkdir /usr/local/nginx/conf/vhost
-cat > /usr/local/nginx/conf/vhost/default.conf <<EOF
-server {
-    listen 1979;
-    server_name localhost;
-    #listen 443 ssl;
-    #ssl_certificate ssl/cert.pem;
-    #ssl_certificate_key ssl/cert.key;
-    charset utf-8;
-    access_log  /data/logs/nginx/default.access.log  main;
-    error_log  /data/logs/nginx/default.error.log;
-    root /data/webroot/;
-    autoindex on;
-    location / {
-        index index.html index.php;
-        try_files $uri $uri/ /index.php?$args;
-    }
-    location ~ .*\.(gif|jpg|jpeg|png|bmp|swf|ico|js|css)$ {
-        expires      30d;
-        access_log off;
-    }
-    location ~ \.php$ {
-        fastcgi_pass 127.0.0.1:9000;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        include fastcgi_params;
-    }
-}
-EOF    
